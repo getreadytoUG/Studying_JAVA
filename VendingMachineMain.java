dee5d458d6a52a9cb9bf3e2f6b5063c2c1c0b198
Hong <wnsvy1237@naver.com>
2023-07-20 16:05:28 +0900

    VendingMachineMain 추가

---
+++ VendingMachineMain.java
@@ -0,0 +1,10 @@
+public class VendingMachineMain{
+    public static void main(String[] args){
+        VendingMachine vm1 = new VendingMachine();
+        VendingMachine vm2 = new VendingMachine();
+
+        String product = vm1.pushProductButton(3);
+        System.out.println(product);
+    }
+
+}
