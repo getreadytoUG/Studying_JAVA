5e124b31ffd88382d47b95ab8ba23f063e53dd71
Hong <wnsvy1237@naver.com>
2023-07-20 16:06:02 +0900

    VendingMachine 추가

---
+++ ObjectiveGrammer/VendingMachine.java
@@ -0,0 +1,10 @@
+public class VendingMachine{
+    // field (가지는 것들)
+    // 생성자
+    // method
+    public String pushProductButton(int menuID){
+        System.out.println(menuID + "를 전달 받았습니다.");
+        return "콜라";
+    }
+    
+}
